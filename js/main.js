9a1f9dff602b5e671f5ba63b128cbeddd514c013
Sam Thomson <samt@samt.st>
2014-03-02 15:58:14 +0100

    delete journey and remove line from map

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
-function initialize() {
-	console.log("initialised");
+function initialize($scope) {
   //directionsDisplay = new google.maps.DirectionsRenderer();
-  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var chicago = new google.maps.LatLng($scope.lat, $scope.lon);
   var mapOptions = {
     zoom:7,
     center: chicago
@@ -23,6 +22,9 @@ app.run(function($rootScope) {
 });
 	
 app.controller('journeys', function($scope){	
+	$scope.lat = 41.85003;
+	$scope.lon = -87.6500523;
+
 	$scope.journeys = [
 		new Journey("copenhagen", "gedser"),
 		new Journey("rostock", "berlin"),
@@ -35,6 +37,10 @@ app.controller('journeys', function($scope){
 		$scope.calculateDistance($scope.journeys.length-1);
 	}
 	
+	$scope.deleteJourney = function(index){
+		$scope.journeys.splice(index, 1);
+	}
+	
 	
     $scope.totalDistance = function () {
 		var iTotal = 0;
@@ -42,9 +48,20 @@ app.controller('journeys', function($scope){
 			if(journey.include)
 				iTotal += journey.distance;
 		});
-		console.log(iTotal);
 		return numberWithCommas(iTotal/1000) + " km";
 	}
+	
+	$scope.redrawMap = function(){
+		initialize($scope);
+		$scope.journeys.forEach(function(journey){
+			journey.render();
+		});
+		// now update the lat/lon values to map pos for the next render
+		$scope.lat = map.getCenter().lat;
+		$scope.lon = map.getCenter().lng;
+		
+	}
+	
 	
 	$scope.calculateDistance = function(index){
 		//$scope.journeys[index].distance();
@@ -56,13 +73,10 @@ app.controller('journeys', function($scope){
 			};
 			directionsService.route(request, function(result, status) {
 				if (status == google.maps.DirectionsStatus.OK) {
-					console.log(result.routes[0].legs[0].distance.value);
+					$scope.journeys[index].result = result;
 					// distance as an absolute number
 					$scope.journeys[index].distance = result.routes[0].legs[0].distance.value;
-					// make directions renderer for this journye
-					$scope.journeys[index].directionsRenderer = new google.maps.DirectionsRenderer({suppressInfoWindows: true, suppressMarkers: true, polylineOptions: {strokeColor: '#FF0000'}});
-					$scope.journeys[index].directionsRenderer.setMap(map);
-      				$scope.journeys[index].directionsRenderer.setDirections(result);
+					//$scope.journeys[index].render();
 					// force ui to update
 					$scope.$apply();
 				}else{
@@ -72,13 +86,24 @@ app.controller('journeys', function($scope){
 	};
 
 	function Journey(start, end){
-		console.log("journey created");
 		// Add object properties like this
 		this.start = start;
 		this.end = end;
 		this.distance = "loading..";
 		this.include = true;
+		
 		//this.distance = calculateDistance();
+		
+		this.render = function(){
+			// make directions renderer for this journye
+			this.directionsRenderer = new google.maps.DirectionsRenderer({suppressInfoWindows: true, suppressMarkers: true, polylineOptions: {strokeColor: '#FF0000'}});
+			if(this.directionsRenderer){
+				this.directionsRenderer.setMap(null);
+			}
+			this.directionsRenderer.setMap(map);
+			if(this.include && this.result)
+				this.directionsRenderer.setDirections(this.result);
+		};
 	}
 });
 
