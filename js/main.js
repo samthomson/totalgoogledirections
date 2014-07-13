68eb9e2a49af7dabef347064668d04f1a6669886
Sam Thomson <samt@samt.st>
2014-07-13 16:10:27 +0200

    fixed a few bugs

--- js/main.js
+++ js/main.js
@@ -2,24 +2,6 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 
 var app = angular.module('myApp', ['google-maps']);
-/*
-	directive('googlePlaces', function(){
-	    return {
-	        restrict:'E',
-	        replace:true,
-	        // transclude:true,
-	        scope: {location:'='},
-	        template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
-	        link: function($scope, elm, attrs){
-	            var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
-	            google.maps.event.addListener(autocomplete, 'place_changed', function() {
-	                var place = autocomplete.getPlace();
-	                $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
-	                $scope.$apply();
-	            });
-	        }
-	    }
-	});*/
 
 app.run(function($rootScope) {
 });
@@ -30,12 +12,12 @@ app.controller('journeys', function($scope){
 	$scope.zoom = 1;
 
 	$scope.map = {
-    center: {
-        latitude: $scope.lat,
-        longitude: $scope.lon
-    },
-    zoom: $scope.zoom
-};
+	    center: {
+	        latitude: $scope.lat,
+	        longitude: $scope.lon
+	    },
+	    zoom: $scope.zoom
+	};
 
 	$scope.journeys = [{start:"copenhagen",end:"roskilde", include: true, key: guid()}];
 	
@@ -43,7 +25,7 @@ app.controller('journeys', function($scope){
 	$scope.addJourney = function() {
 		var sStart = ($scope.journeys.length > 0) ? $scope.journeys[$scope.journeys.length-1].end : "";
 		$scope.journeys.push(new Journey(sStart, "", guid()));
-		$scope.calculateDistance($scope.journeys.length-1);
+		//$scope.calculateDistance($scope.journeys.length-1);
 	}
 	
 	$scope.deleteJourney = function(index){
@@ -60,20 +42,7 @@ app.controller('journeys', function($scope){
 		return numberWithCommas(iTotal/1000) + " km";
 	}
 	
-	/*
-	$scope.redrawMap = function(){
-		initialize($scope);
-		$scope.journeys.forEach(function(journey){
-			journey.render();
-		});
-		// now update the lat/lon values to map pos for the next render
-		$scope.lat = map.getCenter().lat;
-		$scope.lon = map.getCenter().lng;		
-	}
-	*/
-	
 	$scope.calculateDistance = function(index){
-		//$scope.journeys[index].distance();
 		if($scope.journeys[index].start != "" && $scope.journeys[index].end != ""){
 			var request = {
 				origin: $scope.journeys[index].start,
@@ -126,31 +95,6 @@ app.controller('journeys', function($scope){
 		this.path = [{"latitude":0,"longitude":0}];
 		
 		this.lineColour = 'FF0000';
-		
-		//this.distance = calculateDistance();
-		
-		/*
-		this.render = function(){
-
-			this.lineColour = (this.include) ? 'FF0000' : 'C0C0C0';
-			// make directions renderer for this journye
-			this.directionsRenderer = new google.maps.DirectionsRenderer(
-				{
-					suppressInfoWindows: true,
-					suppressMarkers: true,
-					polylineOptions: {strokeColor: '#' + this.lineColour, width:1}
-				}
-			);
-			if(this.directionsRenderer){
-				this.directionsRenderer.setMap(null);
-			}
-			this.directionsRenderer.setMap(map);
-			if(this.result){
-				this.directionsRenderer.setDirections(this.result);
-				//this.result = null;
-			}
-		};
-		*/
 	}
 });
 
